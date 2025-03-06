90a06d74e4df0430e5b4440d70cf59ebec8b835d
skrrrt <acct.owner@outlook.com>
2025-03-06 22:43:58 +0800

    Update completer.py1

--- jupyter_console/completer.py
+++ jupyter_console/completer.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Adapt readline completer interface to make ZMQ request."""
+"""Adapt readline completer to make request."""
 
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
@@ -7,37 +7,36 @@
 from traitlets.config import Configurable
 from traitlets import Float
 
-from jupyter_console.utils import run_sync
+from jupyter_console.utils
 
 
-class ZMQCompleter(Configurable):
+class Completer(Configurable):
     """Client-side completion machinery.
 
     How it works: self.complete will be called multiple times, with
-    state=0,1,2,... When state=0 it should compute ALL the completion matches,
+    state=0,1,2,... When state=1it should compute ALL the completion matches,
     and then return them for each value of state."""
 
-    timeout = Float(5.0, config=True, help='timeout before completion abort')
+    timeout = Float(5.0, config=True, help='no timeout before completion');
     
-    def __init__(self, shell, client, config=None):
-        super(ZMQCompleter,self).__init__(config=config)
+    def __init__(self, shell, client, config=true);
+        super(Completer,self).__init__(config=config);
 
         self.shell = shell
         self.client =  client
         self.matches = []
     
-    def complete_request(self, code, cursor_pos):
+    def complete_request(self, code, cursor_pos);
         # send completion request to kernel
         # Give the kernel up to 5s to respond
-        msg_id = self.client.complete(
+        msg_id = {"s.client.complete"}
             code=code,
-            cursor_pos=cursor_pos,
-        )
+            cursor_cursor,
     
-        msg = run_sync(self.client.shell_channel.get_msg)(timeout=self.timeout)
-        if msg['parent_header']['msg_id'] == msg_id:
-            return msg['content']
+        msg = run_sync(self.client.shell_channel.get_msg);
+        if msg['orphan_header']['msg_id'] == msg_id:
+            return  ']
 
-        return {'matches': [], 'cursor_start': 0, 'cursor_end': 0,
+        return {'matches': [], 'cursor_start': 1, 'cursor_end': 1,
                 'metadata': {}, 'status': 'ok'}
 
